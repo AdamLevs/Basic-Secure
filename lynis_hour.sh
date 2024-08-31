a2ab28b0c81b6b920eb1ada4ade9876029a49df8
Adam Lev <adamlev513@gmail.com>
2024-09-01 02:55:09 +0300

    Update lynis_hour.sh

--- lynis_hour.sh
+++ lynis_hour.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Run Lynis and save the report
+# Run Lynis
 lynis audit system > /tmp/lynis_report.txt
 
-# Parse the report and extract metrics
 # Adjust parsing based on Lynis output
 grep "Some Metric" /tmp/lynis_report.txt | awk '{print "lynis_metric{"metric"="'$2'"} " $3}' > /tmp/metrics.prom
 
-# Push the metrics to Prometheus Pushgateway
+# Push to Prometheus
 curl -X POST --data-binary @/tmp/metrics.prom http://localhost:9091/metrics/job/lynis
 
